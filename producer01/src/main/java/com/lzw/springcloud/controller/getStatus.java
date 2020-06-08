38e2c37c4328079edd1573e2b1c90155b829d917
11632 <zhiwei.liao1@baozun.com>
2020-06-09 00:03:32 +0800

    1

--- producer01/src/main/java/com/lzw/springcloud/controller/getStatus.java
+++ producer01/src/main/java/com/lzw/springcloud/controller/getStatus.java
@@ -1,9 +1,14 @@
 package com.lzw.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @version: 1.0
@@ -18,8 +23,24 @@ public class getStatus {
     @Value("${server.port}")
     private String port;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
     @GetMapping("/ok")
     public String test(){
         return "ok"+port;
+    }
+
+    @GetMapping("/get")
+    public String getPort(){
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = discoveryClient.getInstances("PRODUCER");
+        StringBuffer stringBuffer = new StringBuffer();
+        for (ServiceInstance s :
+                instances) {
+            String host = s.getHost();
+            String s1 = s.getUri().toString();
+            stringBuffer.append(host+"****").append(s1);
+        }
+        return stringBuffer.toString();
     }
 }
--- producer02/src/main/java/springcloud/controller/getStatus.java
+++ producer02/src/main/java/springcloud/controller/getStatus.java
@@ -1,9 +1,15 @@
 package springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @version: 1.0
@@ -15,11 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cloudtest")
 public class getStatus {
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @Value("${server.port}")
     private String port;
 
     @GetMapping("/ok")
     public String test(){
         return "ok"+port;
+    }
+
+    @GetMapping("/get")
+    public String getPort(){
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = discoveryClient.getInstances("PRODUCER");
+        StringBuffer stringBuffer = new StringBuffer();
+        for (ServiceInstance s :
+                instances) {
+            String host = s.getHost();
+            String s1 = s.getUri().toString();
+            stringBuffer.append(host+"****").append(s1);
+        }
+        return stringBuffer.toString();
     }
 }
